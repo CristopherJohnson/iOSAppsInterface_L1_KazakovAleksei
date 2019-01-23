cd1be0ebf73fce8e1f32e7f6af9e67e7c68f73d2
CristopherJohnson <henkok127@mail.ru>
2019-01-23 20:15:25 +0300

    starting work with LoginViewController

---
+++ L1_KazakovAleksei/L1_KazakovAlekseiTests/L1_KazakovAlekseiTests.swift
@@ -0,0 +1,34 @@
+//
+//  L1_KazakovAlekseiTests.swift
+//  L1_KazakovAlekseiTests
+//
+//  Created by Алексей Казаков on 23/01/2019.
+//  Copyright © 2019 Алексей Казаков. All rights reserved.
+//
+
+import XCTest
+@testable import L1_KazakovAleksei
+
+class L1_KazakovAlekseiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
