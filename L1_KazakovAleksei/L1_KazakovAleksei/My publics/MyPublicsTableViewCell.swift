fa27f34c365028ebc8d9517575c414e892f977a9
CristopherJohnson <henkok127@mail.ru>
2019-02-01 01:45:16 +0300

    Done my publics

---
+++ L1_KazakovAleksei/L1_KazakovAleksei/My publics/MyPublicsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyPublicsTableViewCell.swift
+//  L1_KazakovAleksei
+//
+//  Created by Алексей Казаков on 01/02/2019.
+//  Copyright © 2019 Алексей Казаков. All rights reserved.
+//
+
+import UIKit
+
+class MyPublicsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
