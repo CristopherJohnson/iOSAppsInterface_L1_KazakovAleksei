916385aab87af07014eb6363d1fd1f454d22ad0d
CristopherJohnson <henkok127@mail.ru>
2019-01-31 23:09:29 +0300

    Done all publics list

---
+++ L1_KazakovAleksei/L1_KazakovAleksei/Models/PublicsModel.swift
@@ -0,0 +1,10 @@
+//
+//  FriendsModel.swift
+//  L1_KazakovAleksei
+//
+//  Created by Алексей Казаков on 31/01/2019.
+//  Copyright © 2019 Алексей Казаков. All rights reserved.
+//
+
+import Foundation
+
