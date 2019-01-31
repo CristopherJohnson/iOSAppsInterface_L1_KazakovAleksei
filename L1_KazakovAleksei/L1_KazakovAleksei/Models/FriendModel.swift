e11ef371d8342992fcc532b0343e50d27919af3d
CristopherJohnson <henkok127@mail.ru>
2019-02-01 02:28:28 +0300

    Done friends list

---
+++ L1_KazakovAleksei/L1_KazakovAleksei/Models/FriendModel.swift
@@ -0,0 +1,15 @@
+//
+//  FriendModel.swift
+//  L1_KazakovAleksei
+//
+//  Created by Алексей Казаков on 01/02/2019.
+//  Copyright © 2019 Алексей Казаков. All rights reserved.
+//
+
+import Foundation
+
+class Friend {
+    var name: String?
+    var imageName: String?
+    var id: String?
+}
