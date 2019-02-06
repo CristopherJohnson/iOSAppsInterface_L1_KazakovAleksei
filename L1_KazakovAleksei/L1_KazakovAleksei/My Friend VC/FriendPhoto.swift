1f3586fc689aae78d122c6ce5cb4d31c280ed54b
CristopherJohnson <henkok127@mail.ru>
2019-02-06 20:50:48 +0300

    fixed friend image

--- L1_KazakovAleksei/L1_KazakovAleksei/My Friend VC/FriendPhoto.swift
+++ L1_KazakovAleksei/L1_KazakovAleksei/My Friend VC/FriendPhoto.swift
@@ -10,6 +10,8 @@ import UIKit
 
 @IBDesignable class FriendPhoto: UIView {
     
+    weak var imageView: UIImageView?
+    
     @IBInspectable var shadowOffset: CGSize = CGSize() {
         didSet {
             self.layer.shadowOffset = shadowOffset
@@ -38,10 +40,37 @@ import UIKit
     override func awakeFromNib() {
         super.awakeFromNib()
         self.backgroundColor = UIColor.clear
-        self.layer.cornerRadius = frame.size.width / 2
+        self.imageView?.frame = self.bounds
+        self.imageView?.layer.cornerRadius = self.frame.size.width / 2
+        self.imageView?.layer.masksToBounds = true
+    
+
+    }
+    
+    
+    // MARK: - init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubviews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.addSubviews()
         
     }
     
+    // MARK: - subviews
+    
+    private func addSubviews() {
+        if self.imageView == nil {
+            let imageView = UIImageView(image: UIImage(named: "No_Image"))
+            self.addSubview(imageView)
+            self.imageView = imageView
+        }
+    }
+  
     
     /*
     // Only override draw() if you perform custom drawing.
--- L1_KazakovAleksei/L1_KazakovAleksei/My Friend VC/MyFriendTwoTableViewCell.swift
+++ L1_KazakovAleksei/L1_KazakovAleksei/My Friend VC/MyFriendTwoTableViewCell.swift
@@ -11,25 +11,26 @@ import UIKit
 class MyFriendTwoTableViewCell: UITableViewCell {
 
     @IBOutlet weak var friendNameLable: UILabel?
-    @IBOutlet weak var friendImageView: UIImageView?
     var friendId: String?
-    
+    @IBOutlet weak var myFriendImageView: FriendPhoto?
     
     func setFriend(settingFriend: Friend) {
         let name = settingFriend.name ?? "NoName"
         let surname = settingFriend.surname ?? "NoSurname"
         self.friendNameLable?.text = name + " " + surname
-        self.friendImageView?.image = UIImage(named: settingFriend.imageName ?? "No_Image")
         self.friendId? = settingFriend.id ?? "000000"
+        self.myFriendImageView?.imageView?.image = UIImage(named: settingFriend.imageName ?? "No_Image")
     }
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        if let image = friendImageView {
-            image.layer.cornerRadius = image.frame.size.width / 2
-            image.clipsToBounds = true
-        }
-        
-    }
+
+    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        if let image = friendImageView {
+//            image.layer.cornerRadius = image.frame.size.width / 2
+//            image.clipsToBounds = true
+//        }
+//        
+//    }
 
 }
