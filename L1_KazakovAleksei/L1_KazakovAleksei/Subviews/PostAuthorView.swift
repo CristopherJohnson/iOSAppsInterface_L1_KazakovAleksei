7b670baa18b81fd181f708c46c468d0b4fe41e3b
CristopherJohnson <henkok127@mail.ru>
2019-04-26 03:43:56 +0300

    test post presentation

---
+++ L1_KazakovAleksei/L1_KazakovAleksei/Subviews/PostAuthorView.swift
@@ -0,0 +1,21 @@
+//
+//  PostAuthorView.swift
+//  L1_KazakovAleksei
+//
+//  Created by Алексей Казаков on 25/04/2019.
+//  Copyright © 2019 Алексей Казаков. All rights reserved.
+//
+
+import UIKit
+
+class PostAuthorView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ L1_KazakovAleksei/L1_KazakovAleksei/Subviews/WatchCount.swift
@@ -0,0 +1,21 @@
+//
+//  WatchCount.swift
+//  L1_KazakovAleksei
+//
+//  Created by Алексей Казаков on 26/04/2019.
+//  Copyright © 2019 Алексей Казаков. All rights reserved.
+//
+
+import UIKit
+
+class WatchCount: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
